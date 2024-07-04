edecc414e3434bdc588f5305ba994c8be7521d80
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-07-04 20:08:39 +0300

    Small rework

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# this is a simple code for minesweeper game in terminal 
+# Minesweeper-like Game in C
+This project implements a simplified Minesweeper-like game in C, where two players take turns revealing cells on an 8x8 grid. The goal is to avoid selecting cells containing bombs while uncovering safe cells to score points. Players alternate turns until all safe cells are uncovered, and the player with the highest score wins.
+
+## Features
+- Two-player Game: Players take turns selecting cells.
+- Random Bomb Placement: Bombs are randomly placed on the grid at the start of each game.
+- Scoring: Players score points by uncovering safe cells (numbers less than 100).
+- Winning Condition: The game ends when all safe cells have been uncovered, and the player with the higher score is declared the winner.
+## Compilation and Execution
+To compile the game, use the following command:
+```console
+gcc -o minesweeper main.c
+```
+To run the compiled executable:
+```console
+./minesweeper
+```
+## Game Instructions
+1. Players: Enter the names of two players when prompted.
+2. Gameplay:
+- Players take turns entering coordinates (x, y) to uncover cells.
+- Coordinates must be within the range of 0 to 7.
+- The game automatically updates the grid and displays the current score after each move.
+3. End of Game:
+- The game ends when all safe cells are uncovered.
+- The player with the higher score is declared the winner.
+- The game prompts if players want to play again.
+## Future Enhancements
+- Implement additional features such as:
+    - Difficulty levels with different bomb counts.
+    - Interactive menu for game settings.
--- main.c
+++ main.c
@@ -3,131 +3,111 @@
 #include <stdbool.h>
 #include <time.h>
 
-int emtiazpl1(int map[8][8], int x, int y){
-    return map[x][y];
-}
-int emtiazpl2(int map[8][8], int x, int y){
+
+#ifdef _WIN32
+#define CLEAR_SCREEN "cls"
+#else
+#define CLEAR_SCREEN "clear"
+#endif
+
+int emtiazpl1(int map[8][8], int x, int y) {
     return map[x][y];
 }
 
+int emtiazpl2(int map[8][8], int x, int y) {
+    return map[x][y];
+}
 
-int showMap(int show[8][8], int map[8][8], int x, int y){
-    system("cls");
-    
-    
-    
-    
-    for (int a = 0; a < 8; a++){
+int showMap(int show[8][8], int map[8][8]) {
+    system(CLEAR_SCREEN);  // Clear the screen
+
+    for (int a = 0; a < 8; a++) {
         printf("  %d", a);
     }
-    
-    printf("\n"); 
-      
-    for (int i = 0; i < 8; i++){
+    printf("\n");
+
+    for (int i = 0; i < 8; i++) {
         printf("%d", i);
-        for (int j = 0; j < 8; j++){
-            
-            if (show[j][i] == 1){
-                if ((map[j][i]) >= 100){
+        for (int j = 0; j < 8; j++) {
+            if (show[j][i] == 1) {
+                if (map[j][i] >= 100) {
                     printf("[!]");
-                    
-                    
-                    
-                }else{
+                } else {
                     printf("[%d]", map[j][i]);
-                    
-                    
                 }
-            }else{
+            } else {
                 printf("[ ]");
-                
             }
         }
-        
         printf("\n");
-        //printf("-------------------------");
-        //printf("\n");
     }
-    printf("-------------------------");
-    printf("\n");
-    return map[x][y];
-    
-    
+    printf("-------------------------\n");
+    return 0;
 }
-int checkgame(int show[8][8], int map[8][8], int bomb){
-    
-    for (int i = 0; i < 8; i++){
-        for (int j = 0; j < 8; j++){
-            if (show[j][i] == 1){
-                if (map[j][i] >= 100){
-                    bomb ++;
+
+int checkgame(int show[8][8], int map[8][8], int bomb) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (show[j][i] == 1) {
+                if (map[j][i] >= 100) {
+                    bomb++;
                 }
             }
         }
     }
     return bomb;
-    
 }
 
-
-
-
-void checkBomb(int map[8][8], int x, int y){
+void checkBomb(int map[8][8], int x, int y) {
     map[x][y] = 100;
-    if (x - 1 >= 0){
+    if (x - 1 >= 0) {
         map[x-1][y]++;
-        if (y - 1 >= 0){
+        if (y - 1 >= 0) {
             map[x-1][y-1]++;
         }
-        if (y + 1 < 8){
+        if (y + 1 < 8) {
             map[x-1][y+1]++;
         }
     }
-    if (x + 1 < 8){
+    if (x + 1 < 8) {
         map[x+1][y]++;
-        if(y - 1 >= 0){
+        if (y - 1 >= 0) {
             map[x+1][y-1]++;
         }
-        if (y + 1 < 8){
+        if (y + 1 < 8) {
             map[x+1][y+1]++;
         }
     }
-    if (y - 1 >= 0){
+    if (y - 1 >= 0) {
         map[x][y-1]++;
     }
-    if (y + 1 < 8){
+    if (y + 1 < 8) {
         map[x][y+1]++;
     }
-    
-
 }
 
-int select(int show[8][8], int map[8][8], int x, int y){
-    
-    if (x<0 || y<0 || x>=8 || y>=8){
+int my_select(int show[8][8], int map[8][8], int x, int y) {
+    if (x < 0 || y < 0 || x >= 8 || y >= 8) {
         return 0;
     }
-    if (show[x][y] == 1){
+    if (show[x][y] == 1) {
         return 0;
     }
     show[x][y] = 1;
-    if (map[x][y] == 0){
-        
-        select(show, map, x+1, y);
-        select(show, map, x-1, y);
-        select(show, map, x, y+1);
-        select(show, map, x, y-1);
-        select(show, map, x+1, y+1);
-        select(show, map, x+1, y-1);
-        select(show, map, x-1, y+1);
-        select(show, map, x-1, y-1);
-    }    
-    
+    if (map[x][y] == 0) {
+        my_select(show, map, x+1, y);
+        my_select(show, map, x-1, y);
+        my_select(show, map, x, y+1);
+        my_select(show, map, x, y-1);
+        my_select(show, map, x+1, y+1);
+        my_select(show, map, x+1, y-1);
+        my_select(show, map, x-1, y+1);
+        my_select(show, map, x-1, y-1);
+    }
+    return 1; // Indicate successful completion
 }
 
-
-
-int main(){
+int main() {
     int map[8][8] = {0};
     int show[8][8] = {0};
     int bombCount = 15;
@@ -137,91 +117,83 @@ int main(){
     int emtiaz1 = 0, emtiaz2 = 0;
     int win1 = 0, win2 = 0;
     int lvl1 = 0, lvl2 = 0;
-    char a[10000];
     srand(time(NULL));
-    for (int i = 0; i < bombCount; i++){
-        int x = rand()%8;
-        int y = rand()%8;
-        if (map[x][y] < 100){
+
+    // Placing bombs randomly
+    for (int i = 0; i < bombCount; i++) {
+        int x = rand() % 8;
+        int y = rand() % 8;
+        if (map[x][y] < 100) {
             checkBomb(map, x, y);
-        }else{
+        } else {
             i--;
         }
     }
+
     printf("player 1 : ");
     scanf("%s", name1);
     printf("player 2 : ");
     scanf("%s", name2);
-    showMap(show, map, 0, 0);
-    int pl1 = 0, pl2 = 0;
-    
 
-    while (1){
+    showMap(show, map);
+
+    while (1) {
         int x, y;
-        
+
         int res = checkgame(show, map, 0);
-        if (res == 15){
+        if (res == 64 - bombCount) { // All safe cells uncovered
             printf("<Game Over>\n");
-            if (emtiaz1 > emtiaz2){
+            if (emtiaz1 > emtiaz2) {
                 printf("emtiaz %s = %d\n", name1, emtiaz1);
                 printf("emtiaz %s = %d\n", name2, emtiaz2);
                 printf("%s is the winner\n", name1);
-                win1 ++;
-            }else if (emtiaz1 == emtiaz2){
+                win1++;
+            } else if (emtiaz1 == emtiaz2) {
                 printf("Equal\n");
-            }else{
+            } else {
                 printf("emtiaz %s = %d\n", name2, emtiaz2);
                 printf("emtiaz %s = %d\n", name1, emtiaz1);
                 printf("%s is the winner\n", name2);
-                win2 ++;
+                win2++;
             }
-            
-            printf("-------------------------");
+            printf("-------------------------\n");
             break;
         }
-        
-        
-        
-        if (tpl % 2 == 0){   
+
+        if (tpl % 2 == 0) {
             printf("%s ==> EMTIAZ = %d\n", name1, emtiaz1);
-            printf("-------------------------\n");
-            
-        }else{
+        } else {
             printf("%s ==> EMTIAZ = %d\n", name2, emtiaz2);
-            printf("-------------------------\n");
-            
         }
-        
-        //printf("-------------------------");
+        printf("-------------------------\n");
+
         printf("\n");
         printf("enter X : ");
         scanf("%d", &x);
         printf("enter Y : ");
         scanf("%d", &y);
 
-        select(show, map, x, y);
-        showMap(show, map, x, y);
-        
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+            printf("Invalid coordinates. Try again.\n");
+            continue;
+        }
 
-        if (showMap(show, map, x, y) == 0 || showMap(show, map, x, y) < 100){
-            tpl ++;
-        }
-        int w = emtiazpl1(map, x, y);
-        int z = emtiazpl2(map, x, y);
-        if (tpl % 2 == 0){
-            if (w >= 100){
-                emtiaz1 ++;
+        my_select(show, map, x, y);
+        showMap(show, map);
+
+        // Check whose turn it is and update scores
+        if (tpl % 2 == 0) {
+            if (map[x][y] < 100) {
+                tpl++;
+                emtiaz1++;
             }
-        }else{
-            if (z >= 100){
-                emtiaz2 ++;
+        } else {
+            if (map[x][y] < 100) {
+                tpl++;
+                emtiaz2++;
             }
         }
-         
     }
-    
-    
-
 
     return 0;
 }
